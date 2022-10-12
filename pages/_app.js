61cb4b420ba24feb20b04cacddf23b85818cd52b
Li-Chih Yen <lichih.yen@gmail.com>
2022-10-11 22:51:31 -0400

    added the NProgress bar on the head

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,32 @@
-// import '../styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
 import Router from 'next/router'
 import Head from 'next/head'
-import nProgress from 'nprogress'
+import NProgress from 'nprogress'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
+  NProgress.configure({ showSpinner: false })
+
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start()
+  })
+
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done()
+  })
+
   return (
     <>
-      <Head></Head>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css"
+          integrity="sha512-DanfxWBasQtq+RtkNAEDTdX4Q6BPCJQ/kexi/RftcP0BcA4NIJPSi7i31Vl+Yl5OCfgZkdJmCqz+byTOIIRboQ=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+      </Head>
       <ChakraProvider>
         <Layout>
           <Component {...pageProps} />
